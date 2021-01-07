69bb55118cb894d0f4687d77bfe84402e03a7c94
Maunder <maunderscott@gmail.com>
2021-01-07 20:46:06 +1300

    Adding notes and started thinking up the data structure

---
+++ src/app/biome/biome.js
@@ -0,0 +1,9 @@
+// biome id
+// current type id
+
+// types are global and shared between biomes
+
+// biome has types that it can have
+// tile belongs to biome always
+// types belong to one or more biomes
+// the biome is responsible for processing the tiles type and data
