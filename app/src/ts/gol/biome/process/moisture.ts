0de0a5a29952863f90bc25f15b93afd5d333e9fc
Maunder <maunderscott@gmail.com>
2021-01-26 12:48:07 +1300

    Making the trasferRate a modifier type of value

--- app/src/ts/gol/biome/process/moisture.ts
+++ app/src/ts/gol/biome/process/moisture.ts
@@ -3,6 +3,38 @@ import { BiomeTile } from "../biomeTile";
 import { BiomeConfig } from "../loader/biomeLoader";
 import { BiomeTileConfig } from "../loader/biomeTileLoader";
 
+// each tile will be preprocessed for enviroment changes as it will be working
+// from top left to bottom right
+// this mean only the very first tile will not have an already modified state by
+// the time it gets its turn to process this
+// means that it will be pulled from the old state and place into the buffer when
+// it is being processed as a neighbour
+
+const processWaterMovement = (tile: BiomeTile, neighbours: BiomeTile[]) => {
+  const currentLevel = tile.conditions.waterLevel;
+
+  const higher = neighbours.filter(
+    (neighbour) => neighbour.conditions.waterLevel > currentLevel
+  );
+  const lower = neighbours.filter(
+    (neighbour) => neighbour.conditions.waterLevel < currentLevel
+  );
+
+  const getMovement = (neighbour: BiomeTile) => {
+    const level = neighbour.conditions.waterLevel;
+    const force = neighbour.transferRate.water;
+
+    const difference = level - currentLevel;
+
+    return {
+      amount: difference * force,
+    };
+  };
+
+  const positiveEffect = higher.map(getMovement);
+  const negativeEffect = lower.map(getMovement);
+};
+
 export const processMoistureChange = (
   tile: BiomeTile,
   point: Point,
@@ -12,6 +44,8 @@ export const processMoistureChange = (
 ) => {
   const moistureRate = tile.transferRate.moisture;
   const biomeTemp = biome.conditions.temperature;
+
+  const maxWater = tileType.saturation.water;
 
   return;
 };
