e33fbb34e5a050d0f1c1475ca2ee1b1571f61117
abomy <393OL6Zuw1GG>
2021-01-29 11:43:34 +1300

    Reskinned console, will skin more later and added widgets for the side bar

--- app/src/ts/main/index.ts
+++ app/src/ts/main/index.ts
@@ -1,7 +1,44 @@
 import { app, BrowserWindow, Menu } from "electron";
+import {consoleWindow} from '../gol/tools/console'
 
-let win;
 const isDev = process.env.NODE_ENV === "development";
+
+let win: BrowserWindow;
+let debugWindow : BrowserWindow;
+
+
+//clean this later and add it to its own file?
+let menu = Menu.buildFromTemplate([
+  {
+  label: "GOL",
+  submenu: [
+    {
+      label:'Exit',
+      click: function(){
+          win.close();
+      }
+    }
+    ]
+  },
+  {
+    label: "Tools",
+    submenu: [
+      {
+        label:'Dev Tools',
+        click: function(){
+            win.webContents.openDevTools();
+        }
+      },  
+      {
+        label:'Console',
+        click: function(){
+          debugWindow.show();
+        }
+      }
+      ]
+    }
+]);
+
 
 app.on("ready", () => {
   win = new BrowserWindow();
@@ -9,4 +46,7 @@ app.on("ready", () => {
     ? "http://localhost:8080"
     : `file://${__dirname}/index.html`;
   win.loadURL(url);
+  win.setMenu(menu);
+  
+  debugWindow = consoleWindow();
 });
