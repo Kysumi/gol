6327f2b3b394ab81c1b2c5b189952238b103b513
Maunder <maunderscott@gmail.com>
2021-02-10 21:10:06 +1300

    nvm it does matter :upsidedown:

--- app/src/ts/gol/grid/offset.ts
+++ app/src/ts/gol/grid/offset.ts
@@ -16,7 +16,7 @@ const rowOffset = (point: Point, shouldOffset: boolean) => {
   const renderX = x * offset * hexRadius + getOffsetX();
   const renderY = y * ((hexRadius / 4) * 3 + getOffsetY());
 
-  return Point(Math.round(renderX), Math.round(renderY));
+  return Point(renderX, renderY);
 };
 
 export const rowOffestOdd = (point: Point): Point => {
